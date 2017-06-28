ffb78a3681f2873a77ebbbf6cb3637d4bc1d7e81
shawnps <shawn@signalsciences.com>
2017-06-28 14:43:14 +0900

    add ToNetIP test

--- int_test.go
+++ int_test.go
@@ -1,6 +1,7 @@
 package ipv4
 
 import (
+	"bytes"
 	"net"
 	"testing"
 )
@@ -17,5 +18,13 @@ func TestFromDotsError(t *testing.T) {
 	val, err := FromDots("2001:4860:0:2001::68")
 	if err == nil || val != 0 {
 		t.Errorf("Expected error parsing IPv6 string")
+	}
+}
+
+func TestToNetIP(t *testing.T) {
+	n := ToNetIP(3232235777)
+	ip := net.ParseIP("192.168.1.1")
+	if !bytes.Equal(n, ip) {
+		t.Errorf("ToNetIP(3232235777) = %v, want %v", n, ip)
 	}
 }

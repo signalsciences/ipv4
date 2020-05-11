c9882047cee83e3c0b01910e4881560a89fe4665
nickg <nickg@signalsciences.com>
2020-05-11 16:57:32 -0700

    Close #4 - make FromDots be memory-free

--- int.go
+++ int.go
@@ -7,6 +7,8 @@ import (
 	"net"
 )
 
+var ErrBadIP = errors.New("Bad IP address")
+
 // FromNetIP converts a IPv4 net.IP to uint32, error
 func FromNetIP(ip net.IP) (uint32, error) {
 	ip = ip.To4()
@@ -22,22 +24,54 @@ func ToNetIP(val uint32) net.IP {
 		byte(val>>8)&0xFF, byte(val&0xFF))
 }
 
-// FromDots converts a dotted IPv4 address to a uint32
-// http://play.golang.org/p/T5B-6RExlj
-// https://groups.google.com/forum/#!topic/golang-nuts/7sC28I57LRY
-func FromDots(ipAddr string) (uint32, error) {
-	ip := net.ParseIP(ipAddr)
-	if ip == nil {
-		return 0, errors.New("wrong ipAddr format")
+// FromDots converts an IPv4 dotted string into a
+// uint32 (big endian).
+func FromDots(ipstr string) (uint32, error) {
+	var out uint32
+	var oct uint32
+	var num int
+	var hasOct bool
+
+	for i := 0; i < len(ipstr); i++ {
+		b := ipstr[i]
+		switch {
+		case b >= '0' && b <= '9':
+			oct = oct*10 + uint32(b-'0')
+			if oct > 255 {
+				return 0, ErrBadIP
+			}
+			hasOct = true
+		case b == '.':
+			// test for ".." cases
+			if !hasOct {
+				return 0, ErrBadIP
+			}
+			out = (out << 8) | oct
+			oct = 0
+			hasOct = false
+			num++
+
+			// more than 3 dots
+			if num > 3 {
+				return 0, ErrBadIP
+			}
+		default:
+			return 0, ErrBadIP
+		}
 	}
-	ip = ip.To4()
-	if ip == nil {
-		return 0, errors.New("not a IPv4 address")
+
+	// num != 3 --> must have 3 dots
+	// !hasOct --> input ended in a dot or other char
+	if num != 3 || !hasOct {
+		return 0, ErrBadIP
 	}
-	return binary.BigEndian.Uint32(ip), nil
+	out = (out << 8) | oct
+	return out, nil
 }
 
 // ToDots converts a uint32 to a IPv4 Dotted notation
+//
+// Impl is mostly for debugging and is not high performance
 func ToDots(val uint32) string {
 	return fmt.Sprintf("%d.%d.%d.%d",
 		val>>24,
--- int_test.go
+++ int_test.go
@@ -27,3 +27,60 @@ func TestToNetIP(t *testing.T) {
 		t.Errorf("ToNetIP(3232235777) = %v, want %v", n, ip)
 	}
 }
+
+func TestFromDotsErrors(t *testing.T) {
+	cases := []string{
+		"....",
+		"0",
+		"0.",
+		"0.0",
+		"0.0.",
+		"0.0.0",
+		"0.0.0.",
+		"0.0.0.0.",
+		"0..",
+		"0..0",
+		"0.0.0..",
+		"999.1.1.1",
+		"1.1.1.11111",
+	}
+	for _, c := range cases {
+		if _, err := FromDots(c); err == nil {
+			t.Errorf("Case %q did not error", c)
+		}
+	}
+}
+
+func TestRoundTrip(t *testing.T) {
+	cases := []string{
+		"0.0.0.0",
+		"127.0.0.0",
+		"128.0.0.0",
+		"0.127.0.0",
+		"0.128.0.0",
+		"127.127.127.127",
+		"128.128.128.128",
+		"255.255.255.255",
+	}
+
+	for _, c := range cases {
+		raw, err := FromDots(c)
+		if err != nil {
+			t.Errorf("Case %q didn't parse: %s", c, err)
+			continue
+		}
+		if ToDots(raw) != c {
+			t.Errorf("Case %q roundtrip produced %q", c, ToDots(raw))
+		}
+	}
+}
+
+var tempOut uint32
+
+func BenchmarkFromDots(b *testing.B) {
+	var val uint32
+	for i := 0; i < b.N; i++ {
+		val, _ = FromDots("255.129.128.127")
+	}
+	tempOut = val
+}
